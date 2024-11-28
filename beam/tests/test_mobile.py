d1ce7c34773b0761101a194b92f1e38c97ede801
Rohan Bansal <rohan@agritheory.dev>
2024-11-28 17:46:16 +0530

    test: emulate barcode scanner

--- beam/tests/test_mobile.py
+++ beam/tests/test_mobile.py
@@ -6,8 +6,22 @@ import re
 import frappe
 import pytest
 
+from playwright.sync_api import expect
 
-@pytest.fixture(scope="function", autouse=True)
+
+@pytest.fixture(scope="session")
+def browser_context_args(browser_context_args):
+	# emulate an Android barcode scanner
+	return {
+		**browser_context_args,
+		"viewport": {
+			"width": 400,
+			"height": 900,
+		},
+	}
+
+
+@pytest.fixture(autouse=True)
 def login(page):
 	page.set_default_timeout(5000)
 
@@ -18,23 +32,34 @@ def login(page):
 	page.get_by_role("textbox", name="Email").fill("Administrator")
 	page.get_by_role("textbox", name="Password").fill("qwe")
 	page.get_by_role("button", name="Login").click()
+
+	# NOTE: all navigation tests should be done using `expect(page).to_have_url` rather
+	# than `page.expect_navigation()` because the latter does not work with hash-based routes
 	yield
 
 
 def test_ship(page):
+
 	# assert redirection to beam homepage after login
-	page.expect_navigation(url=re.compile("/beam#/"))
+	page.expect_navigation(url=re.compile("/beam"))
 
 	# navigate to ship list page
 	page.get_by_text("Ship").click()
-	page.expect_navigation(url=re.compile("/beam#/ship"))
+	expect(page).to_have_url(re.compile("/beam#/ship"))
 
 	# navigate to first purchase order in list
 	page.get_by_role("listitem").first.click()
-	page.expect_navigation(url=re.compile("/beam#/delivery-note/"))
+	expect(page).to_have_url(re.compile("/beam#/delivery-note"))
 
-	# TODO: scan barcode
+	# TODO: find item via the page
 	barcodes = frappe.get_all(
 		"Item Barcode", filters={"parenttype": "Item", "parent": "Ambrosia Pie"}, pluck="barcode"
 	)
 	assert len(barcodes) > 0
+
+	def scan_request(request):
+		return request.post_data_json.get("cmd") != "beam.beam.scan.scan"
+
+	# TODO: scan barcode
+	with page.expect_request(scan_request) as request_info:
+		page.evaluate("barcode => scanner.simulate(window, barcode)", barcodes[0])
