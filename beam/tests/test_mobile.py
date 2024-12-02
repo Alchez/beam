a223830d918402d6f666e00079647c7f8ba158a1
Rohan Bansal <rohan@agritheory.dev>
2024-12-02 16:30:49 +0530

    fix: generalize comments

--- beam/tests/test_mobile.py
+++ beam/tests/test_mobile.py
@@ -41,24 +41,23 @@ def login(page):
 
 @pytest.mark.parametrize("route", ["Receive", "Ship", "Manufacture"])
 def test_scan_item_barcode(page, route):
-	page.get_by_text(route).click()  # this will redirect to the Ship list page
-	page.locator(
-		"css=.beam_list-item"
-	).first.click()  # this will redirect to the first Purchase Order in the list
+	# navigate in the following order: Home -> List -> Form
+	page.get_by_text(route).click()
+	page.locator("css=.beam_list-item").first.click()
 
-	# find the first item in the order list
+	# find the first item in the list
 	item = page.locator("css=.box .beam_list-item").first
 	item_name, *others = item.inner_text().split("\n")
 	item_count = page.locator("css=.box .beam_item-count").first
 	expect(item_count).to_have_text(re.compile("0/"))
 
-	# ensure the item has barcodes
+	# ensure that the item has barcodes
 	barcodes = frappe.get_all(
 		"Item Barcode", filters={"parenttype": "Item", "parent": item_name}, pluck="barcode"
 	)
 	assert len(barcodes) > 0
 
-	# scan barcode
+	# scan barcode and expect increment by 1
 	with page.expect_request(
 		lambda request: request.headers.get("x-frappe-cmd") == "beam.beam.scan.scan"
 	):
